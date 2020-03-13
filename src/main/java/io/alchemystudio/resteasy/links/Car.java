29e7ee09fa9cc3d4e1776a95346cf85001528ea2
阿男 <l.weinan@gmail.com>
2020-03-13 20:01:03 +0800

    update

--- src/main/java/io/alchemystudio/resteasy/links/Car.java
+++ src/main/java/io/alchemystudio/resteasy/links/Car.java
@@ -1,11 +1,8 @@
 package io.alchemystudio.resteasy.links;
 
-import org.jboss.resteasy.links.RESTServiceDiscovery;
-
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlID;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -14,14 +11,12 @@ import javax.xml.bind.annotation.XmlRootElement;
 // 但是如果访问`ClassLinksProviderService.getForClass()`方法，还是可以
 // 重建基于它的`RESTServiceDiscovery`信息。
 // 具体看`README.md`
-
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.NONE)
 public class Car {
     @XmlID
     @XmlAttribute
     private String name;
-
 
     public String getName() {
         return name;
